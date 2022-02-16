3836646014ebac655fad03305c50acef7df604f1
Bennett Gunson <bengunson@gmail.com>
2022-02-16 12:47:25 -0700

    Start main (map) page and filters

---
+++ FoodBee/Models/Filter.cs
@@ -0,0 +1,10 @@
+﻿
+namespace FoodBee.Models
+{
+    public class Filter
+    {
+        public string Name { get; set; }
+        public string IconSelector { get; set; }
+        public bool Active { get; set; }
+    }
+}
