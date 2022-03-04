6ea4875ce2cef8fe171fdd2c473aa0b494dd40b6
Bennett Gunson <bengunson@gmail.com>
2022-03-04 12:33:32 -0700

    finish up pages for horiontal protoype

---
+++ FoodBee/Models/Vendor.cs
@@ -0,0 +1,11 @@
+﻿namespace FoodBee.Models
+{
+    public class Vendor
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public string ImageUrl { get; set; }    
+
+        public int Booth { get; set; }
+    }
+}
