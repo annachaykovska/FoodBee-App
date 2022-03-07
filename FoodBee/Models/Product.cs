eb9f87b3f155bd0c46493c52338c5949cc2283c2
Bennett Gunson <bengunson@gmail.com>
2022-03-06 18:17:56 -0700

    add list view by location/product button

---
+++ FoodBee/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodBee.Models
+{
+    public class Product
+    {
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+
+
+    }
+}
