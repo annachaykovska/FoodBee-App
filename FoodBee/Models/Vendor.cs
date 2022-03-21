6f9b53fdaa48b9e3b805599a6a3fb429bb71aeb9
Bennett Gunson <bengunson@gmail.com>
2022-03-21 16:10:31 -0600

    dynamic links between product/vendor pages

--- FoodBee/Models/Vendor.cs
+++ FoodBee/Models/Vendor.cs
@@ -5,7 +5,6 @@
         public string Name { get; set; }
         public string Description { get; set; } 
         public string? ImageUrl { get; set; }        // of the form "Images/Vendor/{Name}.png if not specified (remote) example: "Images/Vendor/dd.png
-
         public int Booth { get; set; }      // arbitrary booth number
     }
 }
