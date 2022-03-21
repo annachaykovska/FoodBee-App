542658839f2a1f8ab3d0181d398a4b14f1e7174b
Bennett Gunson <bengunson@gmail.com>
2022-03-21 15:23:54 -0600

    add json data

--- FoodBee/Models/Product.cs
+++ FoodBee/Models/Product.cs
@@ -1,12 +1,28 @@
-﻿namespace FoodBee.Models
+﻿using Newtonsoft.Json;
+
+namespace FoodBee.Models
 {
     public class Product
     {
 
+        public string Vendor { get; set; }        // refers to the vendor who makes this propduct
         public string Name { get; set; }
-        public string Description { get; set; }
         public int Price { get; set; }
+        public string Description { get; set; }
 
+        // Dietary flags
+        public int Glutenfree { get; set; }
+        public int Vegetarian { get; set; }
+        public int Vegan { get; set; }
+        public int Lactose { get; set; }
+        public int Nuts { get; set; }
+        
+
+        // Product type flags
+        private int Food { get; set; }
+        private int Drink { get; set; }
+
+        public string? ImageUrl { get; set; }
 
 
     }
